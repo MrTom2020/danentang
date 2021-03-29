ec2eea0ce4936452f8e8602600acc21cea557f19
MrTom2020 <tomhumchin2@gmail.com>
2021-03-29 19:05:32 +0700

    new login

--- src/Component/Login/Sigin.js
+++ src/Component/Login/Sigin.js
@@ -6,20 +6,33 @@ import {useDispatch} from 'react-redux';
 
 const Balance = () =>
 {
-    /*const dispatch = useDispatch();
-    const [username, setusername] = useState('');
+    const dispatch = useDispatch();
+  const [username, setusername] = useState('');
+   const [password, setpassword] = useState('');
+  const onSubmit = () =>
+  {
+    if (!username || !password)
+    {
+      
+    }  
+  }
     const newTransaction = {
         username
-    };*/
+    };
   return (
     <LinearGradient
       colors={['#FAAD3D', '#EFC90A', '#F1CB0C']}
       style={styles.Box}>
       <View style={{width: '100%',height:'100%', alignItems: 'flex-start'}}>
               <Form>
+                  <Label style={{marginLeft:'30%',fontSize:20,marginBottom:'10%'}}>ĐĂNG NHẬP</Label>
                   <Label for="ten">Tên</Label>
-                  <TextInput style={{ height: 40,fontSize:20 }}
-                  placeholder="Xin vui lòng nhập tên"/>
+                  <TextInput name="ten" style={{ height: 40,fontSize:20 }}
+                      placeholder="Xin vui lòng nhập tên" />
+                   <Label for="matkhau">Mật khẩu</Label>
+                  <TextInput name="ten" style={{ height: 40,fontSize:20 }}
+            placeholder="Xin vui lòng nhập mật khẩu" />
+          <Button title="Đăng Nhập"/>
         </Form>
       </View>
     </LinearGradient>
@@ -29,9 +42,10 @@ const Balance = () =>
 const styles = StyleSheet.create({
   Box: {
     width: '100%',
-    height: 189,
+    height: 300,
     borderRadius: 15,
     flexDirection: 'row',
+    alignItems:'center',
     padding: 22,
   },
 });
--- src/Component/LoginApp.js
+++ src/Component/LoginApp.js
@@ -2,6 +2,7 @@ import { Container } from 'native-base';
 import React from 'react';
 import { View, Text, FlatList, Alert } from 'react-native';
 import Login from './Login/Sigin';
+
 const LoginApp = () =>
 {
     return (
